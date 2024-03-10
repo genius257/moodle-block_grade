94726c5f69ba23bbdcd2daf70b54bd2086d4a06d
Anders Pedersen <hot415@hotmail.com>
2024-03-10 01:11:36 +0000

    Started working on ts support

---
+++ amd/src/index.ts
@@ -0,0 +1,8 @@
+import Templates from "core/templates";
+import Fragment from "core/fragment";//FIXME: importing AMD modules currently does not work...
+//import x from "poopy";
+
+//x.appendNodeContents()
+
+Templates.appendNodeContents(document.getElementById('x'), 'html', 'js');
+//Fragment.
