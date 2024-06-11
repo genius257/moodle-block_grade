fceada7924ec25b0c265ff7744fbb96aa75e0a8a
Anders Pedersen <hot415@hotmail.com>
2024-06-11 21:24:19 +0000

    Added example test file

---
+++ tests/FooBarTest.php
@@ -0,0 +1,13 @@
+<?php
+
+//use advanced_testcase;
+
+use PHPUnit\Framework\TestCase;
+
+class FooBarTest extends TestCase
+{
+    public function testFooBar()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
